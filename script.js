0a1df84c32075fe9d9bd90b97647f10fcbbb8cde
Tricia Brooker <grimmi5894@gmail.com>
2021-10-20 19:03:39 -0400

    created function to add numbers user inputs on submit in script.js, added function to form onsubmit in index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+function addNumbers() {
+  const num1 = parseInt(document.getElementById("number1").value)
+  const num2 = parseInt(document.getElementById("number2").value)
+  const total = num1 + num2
+  alert(total)
+}
